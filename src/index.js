f0c149cd51bcf27c870694c673c8630923def1d4
Aleksander <aleksandergorecki80@gmail.com>
2022-09-26 21:45:08 +0200

    Connect your client to React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "https://graphql.contentful.com/content/v1/spaces/6ofe3ca4bb0x/environments/master?access_token=uQXTOU_cXH35LiyIdYDLLM83gr51dBoCny9h7RbBzLM",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query {
+        portfolioProjectCollection {
+          items {
+            title
+          }
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
